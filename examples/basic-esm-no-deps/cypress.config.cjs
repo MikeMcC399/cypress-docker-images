63840cb810f86aae00f2a35982538d91ab406d7d
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2024-05-16 22:32:42 +0200

    add esm tests

---
+++ examples/basic-esm-no-deps/cypress.config.cjs
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  video: false,
+  fixturesFolder: false,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    supportFile: false,
+  },
+})

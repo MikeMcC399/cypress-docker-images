0f9271fae550cd7c3a25b7c83eca4a10e10f539c
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2024-12-30 14:38:26 +0000

    use 1001 for github actions

--- examples/included-as-non-root/scripts/test.sh
+++ examples/included-as-non-root/scripts/test.sh
@@ -4,4 +4,11 @@ set -e # fail on error
 # Run in examples/included-as-non-root directory
 #
 echo Test cypress/included running under node \(non-root\) user
-docker run --rm -v .:/test -w /test -u node cypress/included
+
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    export DOCKER_USER=1001 # GitHub runner non-root user
+else
+    export DOCKER_USER=node # Cypress Docker image non-root user
+fi
+
+docker run --rm -v .:/test -w /test -u $DOCKER_USER cypress/included

e99e2f9ac30339ca895f004edc5bc348aecf2b8b
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2024-12-30 17:42:42 +0000

    move folders

--- examples/included-as-non-root/cypress.config.js
+++ examples/included-as-non-root/cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   fixturesFolder: false,
+  screenshotsFolder: '/tmp/cypress/screenshots',
+  videosFolder: '/tmp/cypress/videos',
   e2e: {
     supportFile: false,
   },

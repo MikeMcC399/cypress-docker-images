1b13aaf0eadd9ac14c5e4badcd73b651247d0452
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2024-12-30 14:47:11 +0000

    rearrange

--- examples/included-as-non-root/scripts/test.sh
+++ examples/included-as-non-root/scripts/test.sh
@@ -3,12 +3,12 @@ set -e # fail on error
 #
 # Run in examples/included-as-non-root directory
 #
-echo Test cypress/included running under node \(non-root\) user
+
 
 if [ "$GITHUB_ACTIONS" = "true" ]; then
     export DOCKER_USER=1001 # GitHub runner non-root user
 else
     export DOCKER_USER=node # Cypress Docker image non-root user
 fi
-
+echo Test cypress/included running under $DOCKER_USER \(non-root\) user
 docker run --rm -v .:/test -w /test -u $DOCKER_USER cypress/included

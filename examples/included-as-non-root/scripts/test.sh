be830bccca167713edfe4dcbb776320c9b2f56a0
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2024-12-30 13:10:29 +0000

    update non-root test

--- examples/included-as-non-root/scripts/test.sh
+++ examples/included-as-non-root/scripts/test.sh
@@ -4,4 +4,4 @@ set -e # fail on error
 # Run in examples/included-as-non-root directory
 #
 echo Test cypress/included running under node \(non-root\) user
-docker run --rm -v ./src:/test -w /test -u node cypress/included
+docker run --rm -v .:/test -w /test -u node cypress/included

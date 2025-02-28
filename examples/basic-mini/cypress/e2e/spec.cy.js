d335c7377f9ece8cab52538d49a6998ccf9bd87a
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-02-28 17:31:18 +0100

    add failing test to examples/basic-mini

--- examples/basic-mini/cypress/e2e/spec.cy.js
+++ examples/basic-mini/cypress/e2e/spec.cy.js
@@ -1,4 +1,7 @@
 describe('test local demo page', () => {
+  it('will fail', () => {
+    assert.fail('forced failure')
+  })
   it('heading', () => {
     cy.visit('index.html')
     cy.contains('h2', 'Test')
